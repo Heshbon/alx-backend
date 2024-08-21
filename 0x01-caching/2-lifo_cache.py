91bd67c8e85bfbbb23ad240d49cf0ac13ce720b9
Heshbon <cityalight.hesbon@gmail.com>
2024-08-21 14:55:52 +0000

    2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,8 +12,7 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ Assigns item value for the key in self.cache_data dictionary.
-            Discard the most recently added item if the cache
-            exceeds its limit.
+            Discard the last item put in cache (LIFO algorithm)
             Does nothing if key or item is None."""
         if key is not None and item is not None:
             if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and
@@ -24,7 +23,6 @@ class LIFOCache(BaseCaching):
             self.last_key_added = key
 
     def get(self, key):
-        """ Returns the value linked to key in self.cache_data.
-            Returns None if key is None or key doesn't exist
-        """
+        """ Returns the value in self.cache_data linked to key.
+            Returns None if key is None or key doesn't exist"""
         return self.cache_data.get(key, None)

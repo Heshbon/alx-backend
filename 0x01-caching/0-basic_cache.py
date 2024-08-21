53985d37ede409eafe6bfc2ff9f47295ef112057
Heshbon <cityalight.hesbon@gmail.com>
2024-08-21 14:33:08 +0000

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,13 +7,11 @@ class BasicCache(BaseCaching):
     """ Basic caching system that inherits from BaseCaching"""
     def put(self, key, item):
         """ Assigns item value for the key in self.cache_data dictionary.
-            Does nothing if key or item is None.
-        """
+            Does nothing if key or item is None."""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """ Returns the value linked to key in self.cache_data.
-            Returns None if key is None or key doesn't exist.
-        """
+            Returns None if key is None or key doesn't exist."""
         return self.cache_data.get(key, None)

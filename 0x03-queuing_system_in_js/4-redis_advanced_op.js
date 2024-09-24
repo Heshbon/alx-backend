c11d7716e68c62d971e3fd7911d85afe7427da53
Heshbon <cityalight.hesbon@gmail.com>
2024-09-24 13:16:01 +0000

    4-redis_advanced_op.js

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,35 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+const key = 'HolbertonSchools';
+const values = {
+  Portland: 50,
+  Seattle: 80,
+  'New York': 20,
+  Bogota: 20,
+  Cali: 40,
+  Paris: 2
+};
+
+// Stores hash values
+for (const [field, value] of Object.entries(values)) {
+  client.hset(key, field, value, redis.print);
+}
+
+// Displays hash values
+client.hgetall(key, (err, obj) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(obj);
+  }
+});
